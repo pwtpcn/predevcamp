2b1fb8c9490cbec4caebaf7e0883541b1fd5c724
pwtpcn <palmdz08@gmail.com>
2022-11-10 14:13:34 +0700

    first commit

---
+++ src/components/Profile.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="h-14 bg-gradient-to-r from-red-500 to-black flex justify-center items-center h-screen">
+    <div class="min-h-screen flex justify-center items-center">
+        <div class="w-96 h-128 rounded-xl bg-gray-200 flex flex-col">
+            <img class = "w-auto rounded-t-xl" src="../assets/chrome_2021-11-22_20-48-54.png" alt="profile"/>
+            <!-- <div class="h-32 w-32 bg-cover bg-center bg-no-repeat" :style="getUrlImage(image)"></div> -->
+            <div class = "text-center flex flex-col p-2">
+                <span class="text-base font-bold">{{name}}</span>
+                <span class="text-base">{{study}}</span>
+                <span class="text-xs">{{birthday}}</span>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        props:{
+            name:{
+                type: String,
+                default: "---"
+            },
+            study:{
+                type: String,
+                default: "---"
+            },
+            birthday:{
+                type: String,
+                default: "---"
+            },
+            image:{
+                type: String,
+                default:"../assets/chrome_2021-11-22_20-48-54.png"
+            }
+        },
+    method:{
+        getUrlImage(imagePath)
+        {
+            return ({ 'backgroung-image': 'url("./src/'+ imagePath +'")'});
+        },
+    }
+    };
+</script>
+
+<style>
+</style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <profileCard v-for="member in members" :key="member"
+        :image="member.image"
+        :name = "member.name"
+        :study = "member.study"
+        :birthday = "member.birthday"/>
+    </div>
+</template>
+
+<script>
+import profileCard from "../components/Profile.vue"
+export default{
+    data(){
+        return{
+            members:[
+                {
+                    name: "Thanapoom Chaisena",
+                    birthday: "23 Jan 2004",
+                    study: "KU82 CS37",
+                    image: "../assets/IMG_20220430_224331_058.jpg"
+                },
+                {
+                    name:"Kritt Wongwandee",
+                    birthday:"29 Aug 2003",
+                    study: "KU82 CS37"
+                },
+                {
+                    name:"Harit Sombatsiri",
+                    birthday:"24 Nov 2003",
+                    study: "KU82 CS37"
+                }
+            ],
+            
+        }
+    },
+    components:{
+        profileCard,
+    },
+};
+</script>
+
+
+<style>
+
+</style>
